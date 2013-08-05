126666ce4ec12fc27ad550b67de6104416a0263a
Brian McDonald <brian.mcdonald1980@googlemail.com>
2013-08-05 14:04:33 +0100

    Final Version - now with lines

--- app/map.js
+++ app/map.js
@@ -15,7 +15,10 @@ var selected;
 var points=[];
 var layer=null;
 var lineElements=[];
+var textElements=[];
 var selectedPoint=0;
+var lastPoint;
+var point;
 
 
 $(document).ready(function() {
@@ -76,49 +79,55 @@ function displayPointsFromJSON(name)
                 feature.element.setAttribute("text",feature.data.text);
                 feature.element.setAttribute("lon",feature.data.geometry.coordinates[0]);
                 feature.element.setAttribute("lat",feature.data.geometry.coordinates[1]);
+                feature.element.setAttribute("mapX",feature.data.geometry.coordinates.x);
+                feature.element.setAttribute("mapY",feature.data.geometry.coordinates.y);
             }
             var point=points[selectedPoint];
-            point.element.style.fill='red';
-            point.element.setAttribute('r',"10");
+            d3.select(point.element).transition().style('fill','red').attr('r','10');
             map.center({"lat":point.element.getAttribute("lat"), "lon":point.element.getAttribute("lon")});
             showSelectedPoint();
-            //displayLines();
+            displayLines();
+            displayText();
 
         }));
 }
 
 function advancetoPrevPoint()
 {
-    var point=points[selectedPoint];
-    point.element.style.fill='brown';
-    point.element.setAttribute('r',"5");
+    point=points[selectedPoint];
+    lastPoint=points[selectedPoint];
+    d3.select(lastPoint.element).transition().style('fill','brown').attr('r','5');
     selectedPoint--;
     if (selectedPoint<0)
     {
         selectedPoint=points.length-1;
     }
     point=points[selectedPoint];
-    point.element.style.fill='red';
-    point.element.setAttribute('r',"10");
-    //displayLines();
+    d3.select(point.element).transition().style('fill','red').attr('r','10');
+    displayLines();
     showSelectedPoint();
     map.center({"lat":point.element.getAttribute("lat"), "lon":point.element.getAttribute("lon")});
 }
 
 function advanceToNextPoint()
 {
-    var point=points[selectedPoint];
-    point.element.style.fill='brown';
-    point.element.setAttribute('r',"5");
+
+    point=points[selectedPoint];
+    console.log(point);
+
+    lastPoint=points[selectedPoint];
+    //get
+    d3.select(lastPoint.element).transition().style('fill','brown').attr('r','5');
     selectedPoint++;
     if (selectedPoint>points.length-1)
     {
         selectedPoint=0;
     }
     point=points[selectedPoint];
-    point.element.style.fill='red';
-    point.element.setAttribute('r',"10");
-    //displayLines();
+    //point.element.style.fill='red';
+    d3.select(point.element).transition().style('fill','red').attr('r','10');
+    //point.element.setAttribute('r',"10");
+    displayLines();
     showSelectedPoint();
     map.center({"lat":point.element.getAttribute("lat"), "lon":point.element.getAttribute("lon")});
 }
@@ -152,9 +161,42 @@ function displayLines()
         .attr("fill", "none");
 }
 
+function displayText()
+{
+    $("text").remove();
+    for(var i=0;i<points.length;i++)
+    {
+        textElements[i]=transform(points[i].data.geometry.coordinates);
+    }
+
+    var textFunction=d3.svg.text()
+        .x(function(d){return d.x})
+        .y(function(d){return d.y});
+
+    layer.apppend("text")
+        .attr("d",textFunction(textElements))
+        .attr("font-family", "sans-serif")
+        .attr("fill", "black");
+}
+
 function transform(coords) {
+    //do we have a last point and a currentpoint
+    var offsetX=0;
+    var offsetY=0;
+    if (point!=null && lastPoint!=null){
+        var startX=point.element.getAttribute("mapX");
+        var startY=point.element.getAttribute("mapY");
+
+        var endX=lastPoint.element.getAttribute("mapX");
+        var endY=lastPoint.element.getAttribute("mapY");
+
+        offsetX=startX-endX;
+        offsetY=startY-endY;
+        console.log("Offset "+offsetX+" "+offsetY);
+    }
+
     d = map.locationPoint({lon: coords[0], lat: coords[1]});
-    return {"x":d.x, "y":d.y};
+    return {"x":d.x-offsetX, "y":d.y-offsetY};
 }
 
 
